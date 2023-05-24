6d207dba880aa97dedf7ec7b7f79bbe88b0abd02
Katri Pokats <katri.pokats@gmail.com>
2023-05-24 11:56:29 +0300

    Initial commit

---
+++ src/main/java/com/assignment/nl22w/game/impl/GameImpl.java
@@ -0,0 +1,20 @@
+package com.assignment.nl22w.game.impl;
+
+import com.assignment.nl22w.game.Game;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class GameImpl implements Game {
+
+	@Override
+	public int escapeFromTheWoods(Resource resource) throws IOException {
+		//		TODO start your journey here
+
+		return -1;
+	}
+}
